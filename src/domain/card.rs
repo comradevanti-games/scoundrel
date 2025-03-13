10508ee35cf8c1995344118a345d4ddb4143810b
Ramon Brullo <kingvanti650@gmail.com>
2025-03-13 22:50:06 +0700

    fix: card printing

--- src/domain/card.rs
+++ src/domain/card.rs
@@ -66,7 +66,7 @@ pub struct Card {
 
 impl fmt::Display for Card {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}{}", self.rank, self.rank)
+        write!(f, "{}{}", self.rank, self.suite)
     }
 }
 
