30fc8d5810f7fca41d4f9f631a6225dd91798fad
Ramon Brullo <kingvanti650@gmail.com>
2025-03-14 19:22:10 +0700

    refactor: move display logic

--- src/ui/print.rs
+++ src/ui/print.rs
@@ -1,7 +1,48 @@
+use std::fmt::{self};
 use std::io::Write;
 
+use crate::domain::card::{Rank, Suite};
 use crate::domain::{card::Card, game::Game, pile::Pile};
 extern crate termion;
+
+impl fmt::Display for Suite {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let char = match self {
+            Suite::Hearts => 'H',
+            Suite::Clubs => 'C',
+            Suite::Spades => 'S',
+            Suite::Diamonds => 'D',
+        };
+        write!(f, "{}", char)
+    }
+}
+
+impl fmt::Display for Rank {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = match self {
+            Rank::Ace => "A",
+            Rank::Two => "2",
+            Rank::Three => "3",
+            Rank::Four => "4",
+            Rank::Five => "5",
+            Rank::Six => "6",
+            Rank::Seven => "7",
+            Rank::Eight => "8",
+            Rank::Nine => "9",
+            Rank::Ten => "10",
+            Rank::Jack => "J",
+            Rank::Queen => "Q",
+            Rank::King => "K",
+        };
+        write!(f, "{}", s)
+    }
+}
+
+impl fmt::Display for Card {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}{}", self.rank, self.suite)
+    }
+}
 
 fn print_card<W: Write>(stdout: &mut W, x: u16, y: u16, card: &Card) {
     write!(stdout, "{}{}", termion::cursor::Goto(x, y), card).unwrap();
