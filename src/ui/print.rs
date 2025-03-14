15a8805ad6035b397a0cba0897b75c7a697ec067
Ramon Brullo <kingvanti650@gmail.com>
2025-03-14 19:23:16 +0700

    refactor: card display logic

--- src/ui/print.rs
+++ src/ui/print.rs
@@ -44,12 +44,14 @@ impl fmt::Display for Card {
     }
 }
 
-fn print_card<W: Write>(stdout: &mut W, x: u16, y: u16, card: &Card) {
-    write!(stdout, "{}{}", termion::cursor::Goto(x, y), card).unwrap();
-}
-
 fn print_pile<W: Write>(stdout: &mut W, x: u16, y: u16, pile: &Pile) {
-    print_card(stdout, x, y, pile.first().unwrap());
+    write!(
+        stdout,
+        "{}{}",
+        termion::cursor::Goto(x, y),
+        pile.first().unwrap()
+    )
+    .unwrap();
 }
 
 pub fn print_game<W: Write>(stdout: &mut W, game: &Game) {
