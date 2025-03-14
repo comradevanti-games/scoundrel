09f93b816f0842d369494c25534f2d118f0f42bb
Ramon Brullo <kingvanti650@gmail.com>
2025-03-14 19:16:31 +0700

    feat: quit game

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::io::stdout;
+use std::io::{Write, stdin, stdout};
 
 use domain::game::Game;
-use termion::raw::IntoRawMode;
+use termion::{event::Key, input::TermRead, raw::IntoRawMode};
 use ui::print::print_game;
 
 extern crate termion;
@@ -18,9 +18,18 @@ mod ui {
 
 fn main() {
     let mut stdout = stdout().into_raw_mode().unwrap();
+    let stdin = stdin();
 
     let mut rng = rand::rng();
     let game = Game::start_new(&mut rng);
 
     print_game(&mut stdout, &game);
+    stdout.flush().unwrap();
+
+    for key in stdin.keys() {
+        match key.as_ref().unwrap() {
+            Key::Char('q') => break,
+            _ => {}
+        }
+    }
 }
